b48d7ff55f05c1d0a2d60399526e32b986968588
Maninder1220 <maninderjitsinghjaura@gmail.com>
2024-04-03 14:52:48 +0530

    Starting with CICD

---
+++ git-install.sh
@@ -0,0 +1,10 @@
+# Updating Pakages
+sudo apt update
+
+# Git Install
+# git-core/ppa is safe and reputed
+# This code adds PPA to your system.
+sudo add-apt-repository ppa:git-core/ppa -y
+
+# Check Version
+git --version
